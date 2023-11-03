f087a20543ca9f0d388b30a0ba2564e5f73c771d
Robert Synowiec <rsynowiec81@gmail.com>
2023-11-03 19:43:47 +0100

    Add Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import { Container } from 'react-bootstrap';
+import { Routes, Route } from 'react-router-dom';
+import NotFound from './components/pages/NotFound/NotFound';
+import Home from './components/pages/Home/Home';
+import Table from './components/pages/Table/Table';
+
 function App() {
   return (
     <main>
       <Container>
-
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/table/:id" element={<Table />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </Container>
     </main>
   );
